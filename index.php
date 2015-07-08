191a431d4e163d8ccde96a21d0ba0c8fc183df58
CharisZ <github@topbananainternet.com>
2015-07-08 22:44:32 +0100

    Basic holding page

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<html>
+<head>
+	<title>Cox Media Group Dev Site - Nothing to See Here - Move Along Please</title>
+	
+	<style>
+		body {
+			background-color:#000;
+			color:#000;
+			font:12px/14px Verdana, Arial, Helvetica, sans-serif normal;
+			text-align:center;
+		}
+		div {
+			width:400px; height:auto;
+			padding:10px;
+			margin:20px auto;
+			background-color:#bed7fc;
+		}
+		p.small {font-size:9px;}
+	</style>
+</head>
+<body>
+	<div>
+		<h3>Currently Reclaimed, Awaiting Project</h3>
+		<p class="small">(i.e. unused dev site)</p>
+		<p>Nothing to see here, move along please.</p>
+	</div>
+</body>
+</html>
